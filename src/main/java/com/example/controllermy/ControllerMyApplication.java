b577cb5db26f25a503b3adce62fe56b57cdfc7d0
AleksSkyr <greshnik1499@gmail.com>
2023-08-28 02:15:01 +0300

    st

---
+++ src/main/java/com/example/controllermy/ControllerMyApplication.java
@@ -0,0 +1,13 @@
+package com.example.controllermy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@Rest
+@SpringBootApplication
+public class ControllerMyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControllerMyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/controllermy/ControllerMyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.controllermy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControllerMyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
